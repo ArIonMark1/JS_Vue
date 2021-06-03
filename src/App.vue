a9387e53681249ec5ae1ffb5026f23c162ed623a
ArIonMark1 <andreydatsenko777@gmail.com>
2021-06-03 17:27:15 +0200

    lesson6

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,36 @@
 <template>
   <div id="app">
     <div class="header">
+<!-- ========================================== -->
         <div class="top-header">
+
             <div class="buttons-navigations">
                 <input type="text" class="goods-search" v-model="searchLine" />
                 <button class="search-button" type="button" @click="filterGoods">Искать</button>      
             </div>
-        </div>
 
-        <div class="bottom-header">
-            <button class="cart-button" type="button">Корзина</button>
         </div>
+<!-- ========================================== -->
+        <div class="bottom-header">
+
+            <button class="cart-button" type="button" @click="toggleCartStatus">Корзина</button>
+
+        </div>
+<!-- ========================================== -->
     </div>
     
     <main>
+
       Товары:
-      <div class="goods-list">
-        <div v-for="item in filteredGoods" :key="item.id_product" class="goods-item">
-          <h3>{{ item.product_name }}</h3>
-          <p>{{ item.price }}</p>
-          <button class="buy-button"><b>Добавить</b></button>
-        </div>
-      </div>
+      <GoodsList :goods="filteredGoods"/>
       <br />
+      <div v-show="isVisibleCart" class="goods-list"></div>
+      
+      <br />
+
       Корзина:
-      <div class="cart-list">
-        <h3>{{ item.product_name }}</h3>
-          <p>{{ item.price }}</p>
-      </div>
+      <div class="cart-list"></div>
+       
     </main>
   </div>
 </template>
@@ -35,39 +38,49 @@
 // =====================================================
 
 <script>
+import GoodsList from './components/GoodsList';
 
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 
 export default {
   name: 'App',
+  components: {
+    GoodsList,
+  },
 
   data: () => ({
-    goods:  [],
+    goods: [],
     filteredGoods: [],
-    basketGoods: []
+    searhLine: '',
+    isVisibleCart: false,
   }),
-
   mounted() {
     this.makeGETRequest(`${API_URL}/catalogData.json`)
   },
 
   methods: {
-
     makeGETRequest(url) {
-     fetch(url)
-      .then( (data) => data.json() )
-      .then( (data) => {
-        this.goods = data;
-        this.filteredGoods = data;
-      })
+      fetch(url)
+        .then((data) => data.json())
+        .then((data) => {
+          this.goods = data;
+          this.filteredGoods = data;
+        }) 
     },
-  filterGoods() {
-    const regexp = new RegExp(this.searchLine, 'i');
-    this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name))
+    filterGoods() {
+      const regexp = new RegExp(this.searhLine, 'i');
+      this.filteredGoods = this.goods.filter(good => regexp.test(good.product_name));
+    },
+    toggleCartStatus() {
+      this.isVisibleCart = !this.isVisibleCart;
+    }
+  },
+  watch: {
+    searhLine() {
+      this.filterGoods();
+    }
   }
-  }
- 
 }
 </script>
 
